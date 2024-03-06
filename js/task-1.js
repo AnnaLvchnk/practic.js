4414a539309defd225a55fe4fa3df69048914efd
AnnaLvchnk <nlvcnk@gmail.com>
2024-03-06 12:49:22 +0100

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,22 @@
 //написати функцію congratsIfMoreThan100(), яка приймає масив даних(підписників) і виводить на екран за доп. функції alert() 
 //привітання,  якщо у вас більше 100 підписників
-const users = [
-    { name: 'user1', age: 20 },
-    { name: 'user2', age: 21 },
-    { name: 'user3', age: 22 },
-    { name: 'user4', age: 23 },
-    { name: 'user15', age: 24 },
-    { name: 'user16', age: 25 },
-    { name: 'user17', age: 26 },
-    { name: 'user18', age: 27 },
-    { name: 'user19', age: 27 },
-    { name: 'user10', age: 28 },
-];
-function congratsIfMoreThan100(arr) {
-    if (arr.length < 100) {
-        alert('Hello, dear friends');
-    } else {
-        alert('Goodbay')
-    }
-}
-congratsIfMoreThan100(users);
+// const users = [
+//     { name: 'user1', age: 20 },
+//     { name: 'user2', age: 21 },
+//     { name: 'user3', age: 22 },
+//     { name: 'user4', age: 23 },
+//     { name: 'user15', age: 24 },
+//     { name: 'user16', age: 25 },
+//     { name: 'user17', age: 26 },
+//     { name: 'user18', age: 27 },
+//     { name: 'user19', age: 27 },
+//     { name: 'user10', age: 28 },
+// ];
+// function congratsIfMoreThan100(arr) {
+//     if (arr.length < 100) {
+//         alert('Hello, dear friends');
+//     } else {
+//         alert('Goodbay')
+//     }
+// }
+// congratsIfMoreThan100(users);
---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+function squareArr(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] *= arr[i]
+    }
+    return arr;
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(squareArr(numbers))
